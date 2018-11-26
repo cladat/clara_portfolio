946b528956ced13b9076844f6db474bf29d4ee8d
cladat <claradatso@gmail.com>
2018-11-26 11:45:48 +0100

    about me sans filtre

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+$('#circlecss').circleProgress({
+    value: 0.75,
+    size: 80,
+    startAngle: 0,
+    fill: {
+        color: "#073d6e"
+    }
+});
+
+$('#circlehtml').circleProgress({
+    value: 0.85,
+    size: 80,
+    startAngle: 0,
+    fill: {
+        color: "#073d6e"
+    }
+});
+
+$('#circlejs').circleProgress({
+    value: 0.50,
+    size: 80,
+    startAngle: 0,
+    fill: {
+        color: "#073d6e"
+    }
+});
+
+$('#circleps').circleProgress({
+    value: 0.75,
+    size: 80,
+    startAngle: 0,
+    fill: {
+        color: "#073d6e"
+    }
+});
+
+$('#circleph').circleProgress({
+    value: 0.85,
+    size: 80,
+    startAngle: 0,
+    fill: {
+        color: "#073d6e"
+    }
+});
+
+$('#circlevi').circleProgress({
+    value: 0.95,
+    size: 80,
+    startAngle: 0,
+    fill: {
+        color: "#073d6e"
+    }
+});
