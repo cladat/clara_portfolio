9db6be61df97d104334d8fbf2ed6ae90e076246a
cladat <claradatso@gmail.com>
2018-11-30 09:51:53 +0100

    amelioration mobile finie

--- public/js/main.js
+++ public/js/main.js
@@ -227,3 +227,12 @@ $(function () {
         }, 2250);
     }
 });
+
+// Smooth scrolling
+
+$('a[href*=\\#]').on('click', function (event) {
+    event.preventDefault();
+    $('html,body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 500);
+});
